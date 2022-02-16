d7a1adb0e005ca1f6b8b77afa9ed81f4ea9e8413
rashm1n <rashmin.ravindu@gmail.com>
2022-02-16 15:04:59 +0530

    Adding UserRating wrapper and getUserRating method

---
+++ src/main/java/com/rashm1n/ratingsdataservice/model/UserRating.java
@@ -0,0 +1,22 @@
+package com.rashm1n.ratingsdataservice.model;
+
+import java.util.List;
+
+public class UserRating {
+    private List<Rating> userRatings;
+
+    public UserRating(List<Rating> userRatings) {
+        this.userRatings = userRatings;
+    }
+
+    public UserRating() {
+    }
+
+    public List<Rating> getUserRatings() {
+        return userRatings;
+    }
+
+    public void setUserRatings(List<Rating> userRatings) {
+        this.userRatings = userRatings;
+    }
+}
--- src/main/java/com/rashm1n/ratingsdataservice/resources/RatingsResource.java
+++ src/main/java/com/rashm1n/ratingsdataservice/resources/RatingsResource.java
@@ -1,15 +1,29 @@
 package com.rashm1n.ratingsdataservice.resources;
 
 import com.rashm1n.ratingsdataservice.model.Rating;
+import com.rashm1n.ratingsdataservice.model.UserRating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsResource {
-    @GetMapping("/{movieId}") public Rating getRating(@PathVariable("movieId") String movieId) {
+    @GetMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
+    }
+
+    @GetMapping("users/{userId}")
+    public UserRating getusersRatings(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("3456",9)
+        );
+        return new UserRating(ratings);
     }
 }
